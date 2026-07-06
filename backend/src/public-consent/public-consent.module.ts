820ccc06f775b3aab3bc6cefb2ed85d7626aa308
kritika-mathionix <kritika.rajput@mathionix.com>
2026-07-06 10:59:10 +0530

    feat: otp for consent

--- backend/src/public-consent/public-consent.module.ts
+++ backend/src/public-consent/public-consent.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PublicConsentController } from './public-consent.controller';
 import { PublicConsentService } from './public-consent.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { SmsService } from '../notifications/sms.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ConfigModule],
   controllers: [PublicConsentController],
-  providers: [PublicConsentService],
+  providers: [PublicConsentService, SmsService],
 })
 export class PublicConsentModule {}
